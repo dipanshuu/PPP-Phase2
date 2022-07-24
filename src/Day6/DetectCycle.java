fd2fc366279e46b72a8e40e26a417dc664f07855
dipanshuu <dipanshuu0427@gmail.com>
2022-07-24 11:18:40 +0100

    24 July A

---
+++ src/Day6/DetectCycle.java
@@ -0,0 +1,17 @@
+package Day6;
+public class DetectCycle {
+    public boolean hasCycle(ListNode head) {
+        if(head==null || head.next==null){
+            return false;
+        }
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast.next!=null && fast.next.next!=null){
+            fast=fast.next.next;slow=slow.next;
+            if(fast==slow){
+                return true;
+            }
+        }
+        return false;
+    }
+}
